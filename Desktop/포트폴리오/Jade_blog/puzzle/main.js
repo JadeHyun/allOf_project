bbc25f7e7f5c2d44974f5f2530b1826d8959ff20
hyunjun Yang <yhj0439@nate.com>
2022-04-04 18:28:55 +0900

    puzzle 만들기 첫 화면 = 원본 , 2초 후 랜덤으로 이미지를 셔플 까지 완성

---
+++ Desktop/포트폴리오/Jade_blog/puzzle/main.js
@@ -0,0 +1,42 @@
+const container = document.querySelector(".image-container")
+const startButton = document.querySelector(".start-button")
+const gameText = document.querySelector(".game-text")
+const playTime = document.querySelector(".play-time")
+
+const tileCount = 16;
+
+let tiles = []
+
+setGame();
+
+
+function setGame(){
+    tiles = createImageTiles()
+    tiles.forEach(tile=>container.appendChild(tile))
+    setTimeout(() => {
+        container.innerHTML = '';
+        shuffle(tiles).forEach(tile=>container.appendChild(tile))
+    },2000)
+    
+}
+
+function createImageTiles(){
+    const tempArray = []
+    Array(tileCount).fill().forEach( (_, i) => {
+        const li = document.createElement("li");
+        li.setAttribute('data-index',i)
+        li.classList.add(`list${i}`);
+        tempArray.push(li)
+    })
+    return tempArray;
+}
+
+function shuffle(array){
+    let index = array.length -1;
+    while(index >0 ){
+        const randomIndex = Math.floor( Math.random()*(index+1));
+        [array[index],array[randomIndex]] =[array[randomIndex], array[index]]
+        index --;
+    }
+    return array;
+}
