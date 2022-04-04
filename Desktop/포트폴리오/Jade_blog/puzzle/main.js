fd47ffe56d7fe8e31900bb5bc00a8d89234062c2
hyunjun Yang <yhj0439@nate.com>
2022-04-04 19:57:56 +0900

    puzzle game finish

--- Desktop/포트폴리오/Jade_blog/puzzle/main.js
+++ Desktop/포트폴리오/Jade_blog/puzzle/main.js
@@ -6,26 +6,60 @@ const playTime = document.querySelector(".play-time")
 const tileCount = 16;
 
 let tiles = []
+const dragged = {
+    el:null,
+    class:null,
+    index:null
+}
 
-setGame();
+let isPlaying = false ;
+let timeInterval = null;
+let time = 0
 
+
+
+
+//function
+
+function checkStatus() {
+    const currentList = [...container.children];
+    const unMatchedList = currentList.filter((child, index) =>Number(child.getAttribute("data-index")) !== index);
+    console.log(unMatchedList)
+    if(unMatchedList.length === 0){
+       gameText.style.display = 'block'
+       isPlaying = false;
+       clearInterval(timeInterval)
+    }
+}
 
 function setGame(){
+    isPlaying = true;
+    time = 0;
+    container.innerHTML = '';
+    gameText.style.display = 'none'
+    clearInterval(timeInterval)
+    
+
     tiles = createImageTiles()
     tiles.forEach(tile=>container.appendChild(tile))
     setTimeout(() => {
         container.innerHTML = '';
         shuffle(tiles).forEach(tile=>container.appendChild(tile))
+        timeInterval = setInterval(() =>{
+            playTime.innerText = time;
+            time++;
+        },1000)
     },2000)
-    
 }
 
-function createImageTiles(){
+function createImageTiles() {
     const tempArray = []
     Array(tileCount).fill().forEach( (_, i) => {
         const li = document.createElement("li");
-        li.setAttribute('data-index',i)
+        li.setAttribute('data-index', i);
+        li.setAttribute('draggable', 'true');
         li.classList.add(`list${i}`);
+
         tempArray.push(li)
     })
     return tempArray;
@@ -40,3 +74,43 @@ function shuffle(array){
     }
     return array;
 }
+
+
+
+//Events
+container.addEventListener('dragstart', e => {
+    if(!isPlaying) return;
+    const obj =e.target;
+    dragged.el = obj;
+    dragged.class = obj.className;
+    dragged.index = [...obj.parentNode.children].indexOf(obj)
+})
+
+container.addEventListener('dragover', e => {
+    e.preventDefault();
+})
+
+container.addEventListener('drop', e => {
+    if(!isPlaying) return;
+    const obj = e.target;
+    if( obj.className !== dragged.class){
+        let originPlace;
+        let isLast = false;
+    
+        if(dragged.el.nextSibling){
+            originPlace = dragged.el.nextSibling
+        } else {
+            originPlace = dragged.el.previousSibling
+            isLast = true;
+        }
+        
+        const droppedIndex = [...obj.parentNode.children].indexOf(obj);
+        dragged.index > droppedIndex ? obj.before(dragged.el) : obj.after(dragged.el)
+        isLast ? originPlace.after(obj) : originPlace.before(obj)
+    }
+    checkStatus();
+})
+
+startButton.addEventListener('click', () =>{
+    setGame()
+})
